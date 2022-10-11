dd5f064f65669cd0630dc501996c83066abc56fe
Clara Bernadou <clarabernadou56690@gmail.com>
2022-10-11 12:17:02 +0200

    Start lightbox

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -5,23 +5,48 @@ export function mediaFactory(data) {
     function getMediasCardDOM(){
         const article = document.createElement( "article" );
         const div = document.createElement( "div" );
+        const a = document.createElement( "a" );
         const img = document.createElement( "img" );
         const p = document.createElement( "p" );
         const p1 = document.createElement( "p" );
 
+    // -------------------------------------------------------------------------
+
+        // All card
+        article.setAttribute("class", "article-photo");
+
+        // Images
+        a.setAttribute("href", picture);
         img.setAttribute("src", picture);
-        img.setAttribute("alt", `image ${title}`);
+
+        // Photo info
         div.setAttribute('class', 'infoMedia');
         p.textContent = title;
         p1.setAttribute('class', 'likes');
         p1.textContent = likes;
 
-        article.appendChild(img);
+    // -------------------------------------------------------------------------
+
+        article.appendChild(a);
+        a.appendChild(img);
         article.appendChild(div);
         div.appendChild(p);
         div.appendChild(p1);
         return (article);
     }
 
-    return { image, title, likes, getMediasCardDOM }
+    function lightbox() {
+        const lightbox = document.createElement("div");
+
+    // -------------------------------------------------------------------------
+
+        // Lightbox
+        lightbox.setAttribute("class", "lightbox"); 
+
+    // -------------------------------------------------------------------------
+    
+        return(lightbox);
+    }
+
+    return { image, title, likes, getMediasCardDOM, lightbox }
 };
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,13 +11,15 @@ async function init(){
     let photographer = photographers.find(p => p.id == photographerId);
     let myMedia = media.filter(m => m.photographerId == photographerId);
 
+    // Display a photograph in header
     async function displayPhotographerData(photographer){
         const photographHeader = document.querySelector(".photograph-header");
         const photographerModel = photographerFactory(photographer);
         const photographCardDOM = photographerModel.getPhotographCardDOM();
         photographHeader.appendChild(photographCardDOM);
-    }
+    };
 
+    // Display all photographer's media
     async function displayMediaData(myMedia){
         const photographGallery = document.querySelector(".photograph-gallery");
         myMedia.forEach((myMedia) => {
@@ -25,26 +27,43 @@ async function init(){
             const mediaCardDOM = mediaModel.getMediasCardDOM();
             photographGallery.appendChild(mediaCardDOM);
         })
-    }
+    };
 
+    // Display frame with price & likes
     async function displayFrameData(photographer){
         const photographGallery = document.querySelector(".photograph-gallery");
         const frameModel = photographerFactory(photographer);
         const frameCardDOM = frameModel.getFrameDOM();
         photographGallery.appendChild(frameCardDOM);
-    }
+    };
 
+    // Display photographer's name in form contact
     async function displayNameContact(photographer){
         const contactName = document.querySelector(".contactezMoi");
         const contactModel = photographerFactory(photographer);
         const contactNameDOM = contactModel.getNameContact();
         contactName.appendChild(contactNameDOM);
-    }
+    };
+
+    // Display a lightbox when one of the media is clicked
+    async function lightboxMedia(){
+        const lightbox = document.querySelector(".lightbox");
+        const links = document.querySelectorAll("a");
+        console.log(links);
+        
+        for(let link of links){
+            link.addEventListener("click", function(e){
+                e.preventDefault();
+            });
+        }
+    };
 
     displayPhotographerData(photographer);
     displayMediaData(myMedia);
     displayFrameData(photographer);
     displayNameContact(photographer);
+    lightboxMedia(myMedia);
+    
     console.log(myMedia);
 }
 
